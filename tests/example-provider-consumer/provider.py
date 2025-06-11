0b35b76c1590458e6110a8ea54c13d0297ec8333
benkangpeng <benkangpeng@163.com>
2025-06-11 08:30:16 +0800

    [REFACTOR] Move the all registers into `registerFactory.py`

--- tests/example-provider-consumer/provider.py
+++ tests/example-provider-consumer/provider.py
@@ -1,22 +1,4 @@
 from src.server import Server
-from src.register import registerMethod, registerInstance
-
-
-@registerInstance
-class Calculator:
-    def add(self, x, y):
-        return x + y
-
-    def sub(self, x, y):
-        return x - y
-    
-    def mul(self, x, y):
-        return x * y
-    
-    def div(self, x, y):
-        assert y != 0, "Division by zero"
-        return x / y
-
 
 provider = Server.instance()
 
